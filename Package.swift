1ba801d90b85cc9fae9203c46ab168d99cb853e4
Ben Zobrist <ben@zobie.com>
2025-10-23 14:53:00 -0400

    Added Project Files

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mittenz",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "mittenz",
+            targets: ["mittenz"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "mittenz"
+        ),
+        .testTarget(
+            name: "mittenzTests",
+            dependencies: ["mittenz"]
+        ),
+    ]
+)
